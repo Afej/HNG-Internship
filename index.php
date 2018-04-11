721acf48365c2e14f99ecaa916c76cefe1f8b438
Joshua Afekuro <afekurojoshua@gmail.com>
2018-04-11 15:29:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Figma homepage</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="container">
+        <div id="header">
+            <h1>This is FIGMA</h1>
+        </div>
+        <div id=ptext>
+            <p>Glad for this great opportunity HNG Internship.</p>
+            <p>
+                <?php
+                   
+                    // Prints the day, date, month, year, time, AM or PM
+                    echo date("l jS \of F Y h:i:s A") . "<br>";
+
+                ?>
+            </p>        
+        </div>
+
+       
+    </div>
+</body>
+
+</html>
